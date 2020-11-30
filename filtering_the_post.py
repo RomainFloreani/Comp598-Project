b0f7069706544bb8ca2538e655156b69c25a6c54
RomainFloreani <58536116+RomainFloreani@users.noreply.github.com>
2020-11-29 19:59:59 -0500

    Update filtering_the_post.py

--- filtering_the_post.py
+++ filtering_the_post.py
@@ -4,7 +4,7 @@ import pandas as pd
 import re
 import random
 
-# This function gets the titles of all the posts.
+
 
 def get_post_titles(inp,out):
     """
@@ -34,11 +34,6 @@ def write_file_trump(file_in,file_out):
     file_i.close()
     file_o.close()
 
-   
-
-
-
-# We create a random function which randomly selects lines from 3 files.
     
 def chose_random_line(file_in, num_post):
     """
@@ -53,43 +48,25 @@ def chose_random_line(file_in, num_post):
     return list_of_post
 
 
-# For the sample files, I decided to choose at random about the same amount of lines for each of the 3 posts.
-# so we could have an equal representation.
+ def main():
     
-def choose_lines(in_file1,in_file2,in_file3):#, out_file):
-    """
-    
-    """
     list_1= chose_random_line(in_file1,67)
     list_2= chose_random_line(in_file2,66)
     list_3 = chose_random_line(in_file3,66)
-
     big_list = [list_1, list_2,list_3]
-    
     flat_list = []
     for sublist in big_list:
         for item in sublist:
             flat_list.append(item)
     
     list_of_titles = flat_list
-    
-   
-    
-    posts = {'titles': list_of_titles}
+     posts = {'titles': list_of_titles}
     
     df = pd.DataFrame(posts,columns = ['titles'])
     
     df.to_csv('sample_donald.csv', index = False, encoding = 'utf-8')
     
     
-
-    
-
-choose_lines("../data/trump_files/trump_politics_20201118.json","../data/trump_files/trump_politics_20201119.json","../data/trump_files/trump_politics_20201120.json")#"../data/trump_pol_200.csv")
-    
- def main():
-    
-    pass
 
 if __name__ == "__main__":
     main()
