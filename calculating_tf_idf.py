d75496a70603b2f9e15d5cc2e57d175cfa98851e
RomainFloreani <58536116+RomainFloreani@users.noreply.github.com>
2020-11-30 10:32:15 -0500

    Update calculating_tf_idf.py
    Made the code cleaner, mainly optimizing the word_count function. Made a better main. Still a few fixes are needed

--- calculating_tf_idf.py
+++ calculating_tf_idf.py
@@ -1,40 +1,29 @@
 import pandas as pd
-import re
 import json
 from collections import Counter
 import math
+import argparse
+import sys
 
-
-#This code is used to calculate the tf idf scores for the words in each category.
-
-# I upload my conservative and politics sample annotate files.
-conservative = pd.read_csv('con_trump_200.csv')
-
-politics = pd.read_csv('sample_donald_politics.csv')
-
-
-conservative = conservative.drop(['Unnamed: 0'], axis = 1)
-
-
-# I create a merge of the two files. My rationale is for that is that I want to see if one subreddit tf-idf score outweights the other subreddit. 
-
-frames = [conservative, politics]
-merged = pd.concat(frames)
-#print(merged)
-
-
-# I create a function to clean the punctuation. I used the same pattern as for Assignment 9
 def clean_ponctuation(string):
+    """
+    (string) --> string
+    This function is a helper function which takes as input a string and replace some 
+    of its punctuation with whitespace
+    """
     remove = '()[],-.?!:;#&'
     trans = str.maketrans(remove, ' '*len(remove))
     return string.translate(trans)
 
 
-# Just like for assignment 9a. I create a word_count function. Since there are multiple columns,
-# it is slightly different.
 def word_count(df):
-    topics = ['lawsuit','vote count', 'pandemic related','pol opi', 'trump a', 'trump c']
+    """
+    (pandas data frame) --> json dictionary
+    This function takes as input a pandas df. It will return a json dictionary containing
+    all the words and their number of occurences for each topics.
+    """
     
+    topics = ['lawsuit','vote count', 'pandemic related','pol opi', 'trump a', 'trump c']
     df['title'] =df['title'].apply(clean_ponctuation)
     
     word_by_topics = {}
@@ -67,121 +56,123 @@ def word_count(df):
     
     return json_out
 
-word_count(politics)
 
-# After that we have calculated the list of words, I open the 6 large files
-# I run through each file and for simplicity. I add all words to a list of lists
-# where each sublist is all the words from each of the reddit files. PS: This is def not the most efficient method. But it was fast to implement and
-# runs fast.
 def add_words(file1,file2,file3,file4,file5,file6):
+    """
+    (file) --> list
+    Takes as input files and returns a list of lists which contains all the words alpha words 
+    used in each file.
+    """
     list_of_lists = []
-    in_file1 = open(file1,'r') 
-    in_file2  = open(file2,'r') 
-    in_file3  = open(file3,'r') 
-    in_file4  = open(file4,'r') 
-    in_file5  = open(file5,'r') 
-    in_file6  = open(file6,'r') 
-    list1= []
-    list2= []
-    list3= []
-    list4= [] 
-    list5= []
-    list6= []
-    for line in in_file1:
-        words = line.split()
-        
-        for word in words:
-            word = clean_ponctuation(word)
-            if word.isalpha():
-                list1.append(word)
-    list_of_lists.append(list1)
+    list_of_files = [file1,file2,file3,file4,file5,file6]
     
-    for line in in_file2:
-        words = line.split()
-        for word in words:
-            word = clean_ponctuation(word)
-            if word.isalpha():
-                list2.append(word)
-    list_of_lists.append(list2)
-    for line in in_file3:
-        words = line.split()
-        for word in words:
-            word = clean_ponctuation(word)
-            if word.isalpha():
-                list3.append(word)
-    list_of_lists.append(list3)
-    for line in in_file4:
-        words = line.split()
-        for word in words:
-            word = clean_ponctuation(word)
-            if word.isalpha():
-                list4.append(word)
-    list_of_lists.append(list4)
-    for line in in_file5:
-        words = line.split()
-        for word in words:
-            word = clean_ponctuation(word)
-            if word.isalpha():
-                list5.append(word)
-    list_of_lists.append(list5)
-    for line in in_file6:
-        words = line.split()
-        for word in words:
-            word = clean_ponctuation(word)
-            if word.isalpha():
-                list6.append(word)
-    list_of_lists.append(list6)
+    for file_in in list_of_files:
+        words_in_file = []
+        file_to_read = open(file_in,'r')
+        for line in file_to_read:
+            words = line.split()
+            for word in words:
+                if word.isalpha():
+                    words_in_file.append(word)
+    list_of_lists.append(words_in_file)
     
     return list_of_lists
-massive_list = add_words('hot_con_trump18.json','hot_con_trump19.json','hot_con_trump20.json',
-                         'hot_pol_trump18.json','hot_pol_trump19.json','hot_pol_trump20.json')    
 
 
-# This is the same TF_IDF as for the bonus of the assignment . I just added a 1 in the denomiator of the formula to not 
-# a Divide by zero exception.
 def ComputeTF_IDF(data,term,d,list_of_lists):
+    """
+    (this take a json dictionary, a word, a topic, and a list of lists)
+    This will return the tf idf score of a single word.
+    """
+    # For the TF part we start a zero and then add 
+    # all the words which occur
     sum_d = 0
     for w in data[d]:
         sum_d += data[d][w]
-    DF = data[d][term]/sum_d
+    TF = data[d][term]/sum_d
 
     sum_occurences = 0
-    total = 0
     for lists in list_of_lists:
         if term in lists:
             sum_occurences += 1
-            
-    IDF = (6/(sum_occurences+1))
-    return DF * math.log(IDF)
+    if sum_occurences == 0:
+        sum_occurences = 1        
+    IDF = (6/sum_occurences)
+    return TF * math.log(IDF)
+   
+    
 
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('in_file',help = 'the file that will be used as input')
+    parser.add_argument('-p','--in_file2', nargs= "?" ,default ='hey') #how to fix the optinal argument  
+    args = parser.parse_args()
     
-    words_count_dict = json.loads(word_count(conservative))
-    words_dic = {}
-    tf_idf_dict = {}
-    num_words = 10
-    #int(words_count_dict)
+    # We put our csv from task 2 into a dataframe. This csv has been annotated.
+    df = pd.read_csv(args.in_file)
     
-    # I used pandas in ascending order in order to get the max tf-idf scores.
-    for topic in words_count_dict:
+    #Now our code will depend on whether we want to see the two files merged or not!
+    massive_list = add_words('hot_con_trump18.json','hot_con_trump19.json','hot_con_trump20.json',
+                         'hot_pol_trump18.json','hot_pol_trump19.json','hot_pol_trump20.json')
+    if len(sys.argv) == 3:
+        df_optional = pd.read_csv(args.in_file2)
+        frames = [df,df_optional]
+        merged_df = pd.concat(frames)
         
-        tf_idf = {}
-        for word in words_count_dict[topic]:
-            tf_idf[word] = ComputeTF_IDF(words_count_dict,word, topic,massive_list)
-        tf_idf_df = pd.DataFrame.from_dict(tf_idf, orient='index', columns=['TF-IDF'])
-        tf_idf_df.sort_values(by=['TF-IDF'],ascending=False, inplace=True)
-        tf_idf_list = []
-        for i in range(num_words):
-            tf_idf_list.append(tf_idf_df.iloc[i].name)
+        words_count_dict = json.loads(word_count(merged_df))
+        words_dic = {}
+       
+        num_words = 10
         
-        words_dic[topic] = tf_idf_list
-    out = json.dumps(words_dic)
-    out = out.replace('{','{\n')
-    out = out.replace('],','],\n')
-    out = out.replace('}','\n}')
+        for topic in words_count_dict:
+            
+            tf_idf = {}
+            for word in words_count_dict[topic]:
+                tf_idf[word] = ComputeTF_IDF(words_count_dict,word, topic,massive_list)
+            tf_idf_df = pd.DataFrame.from_dict(tf_idf, orient='index', columns=['TF-IDF'])
+            tf_idf_df.sort_values(by=['TF-IDF'],ascending=False, inplace=True)
+            tf_idf_list = []
+            for i in range(num_words):
+                tf_idf_list.append(tf_idf_df.iloc[i].name)
+            
+            words_dic[topic] = tf_idf_list
+        out = json.dumps(words_dic)
+        out = out.replace('{','{\n')
+        out = out.replace('],','],\n')
+        out = out.replace('}','\n}')
+        
+        print("merged")
+        print(out)
+            
+    elif len(sys.argv) == 2:
+        
     
-    print("conservative")       
-    print(out)
+        words_count_dict = json.loads(word_count(df))
+        words_dic = {}
+        
+        num_words = 10
+      
+        
+        for topic in words_count_dict:
+            
+            tf_idf = {}
+            for word in words_count_dict[topic]:
+                tf_idf[word] = ComputeTF_IDF(words_count_dict,word, topic,massive_list)
+            tf_idf_df = pd.DataFrame.from_dict(tf_idf, orient='index', columns=['TF-IDF'])
+            tf_idf_df.sort_values(by=['TF-IDF'],ascending=False, inplace=True)
+            tf_idf_list = []
+            for i in range(num_words):
+                tf_idf_list.append(tf_idf_df.iloc[i].name)
+            
+            words_dic[topic] = tf_idf_list
+        out = json.dumps(words_dic)
+        out = out.replace('{','{\n')
+        out = out.replace('],','],\n')
+        out = out.replace('}','\n}')
+        
+        print("single")
+        print(out)
 
 if __name__ == '__main__':
     main()
